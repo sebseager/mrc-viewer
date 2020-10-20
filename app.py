227a8b4e6b56fae48d36cda054314718c26af843
Seb Seager <seb.seager@gmail.com>
2020-10-20 01:57:35 -0500

    Debug mode off

--- app.py
+++ app.py
@@ -472,5 +472,5 @@ try:
     print("Detected IPython environment (running inline)")
 except Exception:
     if __name__ == '__main__':
-        app.run_server(debug=True)  # TODO: set to False before deploying
+        app.run_server(debug=False)
         print("Running in server mode")
